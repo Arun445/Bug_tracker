cb1c9ccd93942eb81fae005a47cac437c73092ea
root <root@DESKTOP-7UV7VMH.localdomain>
2021-09-23 15:01:24 +0300

    gitlab testsasa

--- app/bug_tracker/settings.py
+++ app/bug_tracker/settings.py
@@ -19,14 +19,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+# 9g$tf*(scqd0_(mxt7_i@ge9swl)_vzczq^)_*dx$=^k017sg)
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+try:
+    SECRET_KEY = config('SECRET_KEY')
+    DEBUG = config('DEBUG', default=False, cast=bool)
+    ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
+except:
+    SECRET_KEY = '9g$tf*(scqd0_(mxt7_i@ge9swl)_vzczq ^)_*dx$= ^k017sg)'
+    DEBUG = True
+    ALLOWED_HOSTS = []
 # SECURITY WARNING: don't run with debug turned on in production!
-
-DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
