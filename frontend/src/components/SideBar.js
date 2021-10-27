da95bc77b7542a4def88e4c132ab7c3718d74a6c
aron <arunas.martinaitis1@gmai.com>
2021-10-28 02:16:36 +0300

    dashboard in progress

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SideBar() {
-  return <div>SideBar</div>;
+  const [open, setOpen] = useState(true);
+
+  return (
+    <nav>
+      <div className={!open ? "sidebar" : "sidebar active"}>
+        <div className="sidebar-open" onClick={() => setOpen(!open)}>
+          {open ? (
+            <i className="fas fa-angle-left"></i>
+          ) : (
+            <i className="fas fa-angle-right"></i>
+          )}
+        </div>
+        <ul>
+          <li>
+            <a href="#">
+              <i className="fas fa-columns"></i>Dashboard
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fas fa-clipboard-list"></i>My Tickets
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fas fa-tasks"></i>Projects
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fas fa-users"></i>Users
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default SideBar;
--- frontend/src/screens/DashBoard.js
+++ frontend/src/screens/DashBoard.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function DashBoard() {
-  return (
-    <div>
-      <a></a>
-    </div>
-  );
+  return <div className="dashboard">WHAT UPPP</div>;
 }
 
 export default DashBoard;
