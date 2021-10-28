2ccab56eb94cd78df951089415b6d87a2f4aa1eb
aron <arunas.martinaitis1@gmai.com>
2021-10-29 01:46:11 +0300

    front

--- frontend/src/screens/DashBoard.js
+++ frontend/src/screens/DashBoard.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function DashBoard() {
-  return <div className="dashboard">WHAT UPPP</div>;
+  return (
+    <div className="main">
+      <h2>Dashboard</h2>
+
+      <div className="dashboard-box"></div>
+      <div className="dashboard-diagrams">
+        <div className="dashboard-box-sm"></div>
+        <div className="dashboard-box-sm"></div>
+        <div className="dashboard-box-sm"></div>
+      </div>
+    </div>
+  );
 }
 
 export default DashBoard;
